4d215a1f01cbcdaff6bf1b29168bd7de6a7132c1
Joe Delfino <jdelfino@brandeis.edu>
2024-10-22 15:36:25 +0000

    move ns sequence solution over

--- exercises/06/03_number_sum_sequence/code.py
+++ exercises/06/03_number_sum_sequence/code.py
@@ -1,23 +1,34 @@
-def ns_sequence(start, n):
-    for i in range(n):
-        digit_sum = 0
-        for c in str(start):
-            digit_sum += int(c)
-        start = digit_sum * (i + 1)
-    return start
 
-def test_example1():
-    assert ns_sequence(153, 0) == 153
-    assert ns_sequence(153, 1) == 9
-    assert ns_sequence(153, 2) == 18
-    assert ns_sequence(153, 3) == 27
-    assert ns_sequence(153, 4) == 36
+# define function
+def ns_sequence(start, n):
+    lastNum = start
+    all_nums = ''
+
+    # for loop over 'n' - the number of digits
+    for i in range(n):
+        sum = 0
+        # for loop over every character in the number
+        for c in str(lastNum):
+            # keep a running sum of the digits
+            sum += int(c)
+        
+        # multiply by the number of numbers generated so far
+        sum *= i + 1
+        lastNum = sum
+        all_nums += '\t' + str(lastNum)
+
+    # return the last number
+    return int(lastNum)
+
+if __name__ == "__main__":
+    start = int(input("Give me a starting number: "))
+    num = int(input("How many numbers should I generate? "))
+
+    print(ns_sequence(start, num))
+
+def test_example():
     assert ns_sequence(153, 5) == 45
 
 def test_example2():
-    assert ns_sequence(2731, 0) == 2731
-    assert ns_sequence(2731, 1) == 13
-    assert ns_sequence(2731, 2) == 8
-    assert ns_sequence(2731, 3) == 24
-    assert ns_sequence(2731, 4) == 24
     assert ns_sequence(2731, 5) == 30
+
---
+++ exercises/06/03_number_sum_sequence/main.py
@@ -0,0 +1,6 @@
+import code
+
+start = int(input("Give me a starting number: "))
+num = int(input("How many numbers should I generate? "))
+
+print(code.ns_sequence(start, num))
